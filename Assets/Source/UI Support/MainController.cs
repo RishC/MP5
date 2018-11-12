1acbc030cdd11c3aa1e392136442683eecfb8a7c
RishC <rishc94@gmail.com>
2018-11-11 16:11:55 -0800

    initialize ui

---
+++ Assets/Source/UI Support/MainController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainController : MonoBehaviour {
+
+    public TheWorld MainWorld = null;
+    public Camera MainCamera = null;
+    public XfromControl XFormControl = null;
+    public MeshController MeshControl = null;
+    public CylinderController CylinderControl = null;
+    public Dropdown ObjectDropdown = null;
+
+	// Use this for initialization
+	void Start () {
+        ShowMesh();
+        InitObjectDropdown();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ShowMesh()
+    {
+        MeshControl.gameObject.SetActive(true);
+        CylinderControl.gameObject.SetActive(false);
+    }
+
+    void ShowCylinder()
+    {
+        MeshControl.gameObject.SetActive(false);
+        CylinderControl.gameObject.SetActive(true);
+    }
+
+    void InitObjectDropdown()
+    {
+        ObjectDropdown.onValueChanged.AddListener(ObjectChange);
+    }
+
+    void ObjectChange(int value)
+    {
+        if (value == 0)
+        {
+            ShowMesh();
+        }
+        if (value == 1)
+        {
+            ShowCylinder();
+        }
+    }
+}
